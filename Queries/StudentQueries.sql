f90853ac25411197b6337548e5ac72836bd7b0a3
Vasilina Vlasova <vasenysh@gmail.com>
2014-11-30 17:50:55 -0500

    Queries for the menu

--- Queries/StudentQueries.sql
+++ Queries/StudentQueries.sql
@@ -1,2 +1,21 @@
-SELECT ALL * FROM Student;
-SELECT FirstName FROM Student;
+--See all students
+SELECT * FROM Students;
+
+--Create a new student entry
+INSERT INTO Students									
+VALUES(newSNumber, newFirstName, newLastName, newSemester, newDays, newTime, newGender, newPhoneNumber);
+
+--see courses requested by a student
+SELECT StudentNumber, CoursesNeeded
+FROM StCourses
+WHERE StudentNumber=ourStudentVar;
+
+--see prefered days and times by a student
+SELECT StudentNumber, PreferredCourseDays, PreferredCourseTimes
+FROM Students
+WHERE StudentNumber=ourStudentVar;
+
+--see term schedule by a student
+SELECT SectionNumber, CourseNumber, DaysOfTheWeek, StudentEnrolled
+FROM Sections, StSection
+WHERE StudentEnrolled=ourStudentVar AND SectionNumber=StSection.Section;
