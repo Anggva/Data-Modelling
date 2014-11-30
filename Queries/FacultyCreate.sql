56ada9cf7f9485232ab9917e43ac328b308d65b4
Vasilina Vlasova <vasenysh@gmail.com>
2014-11-30 03:39:23 -0500

    Insert and create statements as on Osprey

--- Queries/FacultyCreate.sql
+++ Queries/FacultyCreate.sql
@@ -5,7 +5,6 @@ CREATE TABLE FacultyMembers(
 	LastName CHAR(15),
 	Position CHAR(20),
 	AreaOfExpertise CHAR(20),
-	CoursesTaught CHAR(10),
 	PreferredCourseDays CHAR(5),
 	PreferredCourseTimes CHAR(25),
 	Gender CHAR(1),
@@ -13,20 +12,3 @@ CREATE TABLE FacultyMembers(
 	isAdministrator CHAR(1),
 	Password CHAR(15)
 	);
---add foreign key
-ALTER TABLE Faculty
-ADD FOREIGN KEY (CoursesTaught)
-REFERENCES Course(CourseNumber);
-
-
-alter table Faculty
-drop constraint CoursesTaught_fk;
-
-alter table Section
-add constraint FacultyTeaching
-foreign key(continent)
-references continent
-  5    on delete cascade;
-
-alter table FacultyMembers
-add PrefFormNumber CHAR(8);
--- Queries/StudentCreate.sql
+++ Queries/StudentCreate.sql
@@ -2,11 +2,25 @@ CREATE TABLE Students(StudentNumber CHAR(9) PRIMARY KEY,
 	FirstName CHAR(15),
 	LastName CHAR(15),
 	Semester CHAR(7),
-	CoursesNeeded CHAR(10),
 	PreferredCourseDays CHAR(5),
 	PreferredCourseTimes CHAR(25),
 	Gender CHAR(1),
 	PhoneNumber CHAR(10)
 	);
 
-	FOREIGN KEY (CoursesNeeded) REFERENCES Course(CourseNumber)
+CREATE TABLE StCourses (
+	StudentNumber CHAR(9),
+	CoursesNeeded CHAR(10)
+);
+
+ALTER TABLE StCourses
+ADD CONSTRAINT fk_Courses
+FOREIGN KEY(CoursesNeeded)
+REFERENCES Courses(CourseNumber)
+ON DELETE CASCADE;
+
+ALTER TABLE StCourses
+ADD CONSTRAINT fk_Students
+FOREIGN KEY(StudentNumber)
+REFERENCES Students(StudentNumber)
+ON DELETE CASCADE;
